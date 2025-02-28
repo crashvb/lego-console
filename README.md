447b509d8e9f664c418ba2fc50df08f522d37c59
Richard Davis <crashvb@gmail.com>
2025-02-28 15:33:09 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,45 @@
+# lego-console
+
+[![pypi version](https://img.shields.io/pypi/v/lego-console.svg)](https://pypi.org/project/lego-console)
+[![build status](https://github.com/crashvb/lego-console/actions/workflows/main.yml/badge.svg)](https://github.com/crashvb/lego-console/actions)
+[![coverage status](https://coveralls.io/repos/github/crashvb/lego-console/badge.svg)](https://coveralls.io/github/crashvb/lego-console)
+[![python versions](https://img.shields.io/pypi/pyversions/lego-console.svg?logo=python&logoColor=FBE072)](https://pypi.org/project/lego-console)
+[![linting](https://img.shields.io/badge/linting-pylint-yellowgreen)](https://github.com/PyCQA/pylint)
+[![code style](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![license](https://img.shields.io/github/license/crashvb/lego-console.svg)](https://github.com/crashvb/lego-console/blob/master/LICENSE.md)
+
+## Overview
+
+Console for Lego Mindstorm Inventor / Spike Prime.
+
+Refactored from [recursivetree/spiketools](https://github.com/recursivetree/spiketools).
+
+## Installation
+### From [pypi.org](https://pypi.org/project/lego-console/)
+
+```
+$ pip install lego-console
+```
+
+### From source code
+
+```bash
+$ git clone https://github.com/crashvb/lego-console
+$ cd lego-console
+$ virtualenv env
+$ source env/bin/activate
+$ python -m pip install --editable .[dev]
+```
+
+## Usage
+
+TODO
+
+### Environment Variables
+
+| Variable | Default Value | Description |
+| ---------| ------------- | ----------- |
+
+## Development
+
+[Source Control](https://github.com/crashvb/lego-console)
---
+++ lego_console/cli.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+"""xsessionp command line interface."""
+
+import logging
+import sys
+
+from traceback import print_exception
+from typing import NamedTuple
+
+import click
+
+from click.core import Context
+from crashvb_logging_utilities import (
+    LOGGING_DEFAULT,
+    logging_options,
+    set_log_levels,
+)
+
+from .lego_console import LegoConsole
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TypingContextObject(NamedTuple):
+    # pylint: disable=missing-class-docstring
+    lego_console: LegoConsole
+    verbosity: int
+
+
+def get_context_object(*, context: Context) -> TypingContextObject:
+    """Wrapper method to enforce type checking."""
+    return context.obj
+
+
+@click.group()
+@click.option(
+    "--auto-connect/--no-auto-connect",
+    default=True,
+    help="Toggles connecting to a device automatically.",
+    show_default=True,
+)
+@logging_options
+@click.pass_context
+def cli(
+    context: Context,
+    auto_connect: bool = True,
+    verbosity: int = LOGGING_DEFAULT,
+):
+    """A declarative window instantiation utility based on xsession."""
+
+    if verbosity is None:
+        verbosity = LOGGING_DEFAULT
+
+    set_log_levels(verbosity)
+
+    context.obj = TypingContextObject(
+        lego_console=LegoConsole(auto_connect=auto_connect), verbosity=verbosity
+    )
+
+
+@cli.command(name="start", short_help="Starts an interactive lego console.")
+@click.pass_context
+def start(context: Context):
+    # pylint: disable=protected-access,redefined-builtin
+    """Starts a lego console."""
+    ctx = get_context_object(context=context)
+    try:
+        ctx = get_context_object(context=context)
+        ctx.lego_console.cmdloop()
+    except Exception as exception:  # pylint: disable=broad-except
+        if ctx.verbosity > 0:
+            logging.fatal(exception)
+        if ctx.verbosity > LOGGING_DEFAULT:
+            exc_info = sys.exc_info()
+            print_exception(*exc_info)
+        sys.exit(1)
+
+
+@cli.command()
+def version():
+    """Displays the version."""
+
+    # Note: * This cannot be imported above, as it causes a circular import!
+    #       * This requires '__version__' to be defined in '__init__.py'
+    from . import __version__  # pylint: disable=import-outside-toplevel
+
+    print(__version__)
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    cli()
